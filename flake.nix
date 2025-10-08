bac377da896c98560024185613697f9ac6795036
Megumori <morialeo@proton.me>
2025-10-08 20:08:32 +0300

    update zen browser

--- flake.nix
+++ flake.nix
@@ -8,8 +8,10 @@
 
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-25.05";
+    };
+    
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
