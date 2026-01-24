a420f5054f404ac89365f3d6681081223702c9dc
Megumori <morialeo@proton.me>
2026-01-24 19:06:26 +0200

    Bluetooth for laptop

--- hosts/MeguFW12/default.nix
+++ hosts/MeguFW12/default.nix
@@ -17,20 +17,30 @@
 	#framework update
   services.fwupd.enable = true;
 
-# 	  # Bootloader.
-# 	  boot.loader.grub.enable = true;
-# 	  boot.loader.grub.device = "/dev/sda";
-# 	  boot.loader.grub.useOSProber = true;
-#
-# 	  # Setup keyfile
-# 	  boot.initrd.secrets = {
-# 	    "/boot/crypto_keyfile.bin" = null;
-# 	  };
-#
-# 	  boot.loader.grub.enableCryptodisk = true;
-#
-# 	  boot.initrd.luks.devices."luks-d2e16093-6af1-4725-a638-7eeea49fe565".keyFile =
-# 	    "/boot/crypto_keyfile.bin";
+	#Bluetooth
+	hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = true;
+		settings = {
+			General = {
+				# Shows battery charge of connected devices on supported
+				# Bluetooth adapters. Defaults to 'false'.
+				Experimental = true;
+				# When enabled other devices can connect faster to us, however
+				# the tradeoff is increased power consumption. Defaults to
+				# 'false'.
+				FastConnectable = true;
+			};
+			Policy = {
+				# Enable all controllers when they are found. This includes
+				# adapters present on start as well as adapters that are plugged
+				# in later on. Defaults to 'true'.
+				AutoEnable = true;
+			};
+		};
+	};
+	services.blueman.enable = true;
+
 #
 	  nixpkgs.config.allowUnfree = true;
 
