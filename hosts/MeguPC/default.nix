e5546cc75342c9a72be45fb8977e75a3b924df8a
Megumori <morialeo@proton.me>
2026-02-23 08:37:19 +0200

    grub bootloader

--- hosts/MeguPC/default.nix
+++ hosts/MeguPC/default.nix
@@ -17,29 +17,37 @@
   ../../modules/plymouth.nix #Boot animation
   ];
 
-  boot.loader = {
-	systemd-boot.enable = true;
-	efi.canTouchEfiVariables = true;
-  };
-
-	# Needed to find the USB device during initrd stage
-  boot.initrd.kernelModules = [ "usb_storage" ]; 
-
-  boot.initrd.luks.devices = {
-	luksroot = {
-    	device = "/dev/disk/by-uuid/400fdfd6-e31e-4ead-b02c-8ad1d6b89ac4";
-	  allowDiscards = true;
-      keyFileSize = 4096;
-      keyFile = "/dev/sdb";
-   	  #fallbackToPassword = true; #unnecessary due to initrd.systemd in plymouth.nix
+  boot = {
+    loader = {
+	    efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
       };
-	cryptstorage = {
-	  device = "/dev/disk/by-uuid/ce9c8ab1-54a5-45b5-b566-7eaec2d50948";
-	  allowDiscards = true;
-	  keyFileSize = 4096;
-	  keyFile = "/dev/sdb";
-      #allbackToPassword = true; #unnecessary due to initrd.systemd in plymouth.nix
-	};
+    };
+
+    initrd = {
+      # Needed to find the USB device during initrd stage
+      kernelModules = [ "usb_storage" ]; 
+
+      luks.devices = {
+        luksroot = {
+            device = "/dev/disk/by-uuid/400fdfd6-e31e-4ead-b02c-8ad1d6b89ac4";
+            allowDiscards = true;
+            keyFileSize = 4096;
+            keyFile = "/dev/sdb";
+            #fallbackToPassword = true; #unnecessary due to initrd.systemd in plymouth.nix
+        };
+        cryptstorage = {
+          device = "/dev/disk/by-uuid/ce9c8ab1-54a5-45b5-b566-7eaec2d50948";
+          allowDiscards = true;
+          keyFileSize = 4096;
+          keyFile = "/dev/sdb";
+            #allbackToPassword = true; #unnecessary due to initrd.systemd in plymouth.nix
+	      };
+      };
+    };
   };
 
   # Automount 2nd ssd
