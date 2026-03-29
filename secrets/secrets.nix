c89f33862a58eec59c781dedcd6d9d7d9c3fc34d
Megumori <morialeo@proton.me>
2026-03-29 09:28:10 +0300

    laptop to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,14 @@
 let
-  megumori = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK49+/ydcGoZLq+EiWS4xoCiBphAyLL9bfvTN9iLnZ7+";
-  users = [ megumori ];
+  megumoriPC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK49+/ydcGoZLq+EiWS4xoCiBphAyLL9bfvTN9iLnZ7+";
+  megumoriFW12 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICP8IfIrstLFPER4YVwCg1aTXTRZ/FhXJ+/mbWdKpoI4";
+  users = [ 
+    megumoriPC
+    megumoriFW12
+  ];
+  megumori = [
+    megumoriPC
+    megumoriFW12
+  ];
 
 
   MeguPC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZe2gvxMMBHVdUrszMe+wtEaazjq9+XicnzF+g82T2G";
@@ -9,7 +17,7 @@ in
 {
 
   # Syncthing keys setup (mainly for trying out agenix)
-  "syncthing/key.pem.age".publicKeys = [ megumori ];
-  "syncthing/cert.pem.age".publicKeys = [ megumori ];
+  "syncthing/key.pem.age".publicKeys = megumori;
+  "syncthing/cert.pem.age".publicKeys = megumori;
 
 }
